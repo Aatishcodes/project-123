07c3f841631fdfb9882fb174c0551fa0deb9af26
Aatishcodes <118838033+Aatishcodes@users.noreply.github.com>
2023-11-30 20:31:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup() {
+    canvas = createCanvas(500, 500)
+    canvas.position(900, 370)
+    video = createCapture(VIDEO);
+    video.position(10,370)
+    video.size(1000,500)
+    //video.hide();
+}
+
+
+function draw() {
+    //image(video,0,0,500,500)
+}
